10918f66a67df8da9d497fb415bd0b784af4df80
Fabio Spampinato <spampinabio@gmail.com>
2020-11-06 19:08:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# nsfw-prebuild-electron
+
+Prebuild version of [NSFW](https://github.com/Axosoft/nsfw) for Electron.
+
+Each published version corresponds to the Electron version binaries are build for.
---
+++ binary.js
@@ -0,0 +1,26 @@
+
+const os = require ( 'os' );
+const platform = os.platform ();
+const arch = os.arch ();
+
+if ( platform === 'darwin' && arch === 'x64' ) {
+
+  module.exports = require ( './binaries/darwin-x64.node' );
+
+} else if ( platform === 'win32' && arch === 'x64' ) {
+
+  module.exports = require ( './binaries/win32-x64.node' );
+
+} else if ( platform === 'win32' && arch === 'ia32' ) {
+
+  module.exports = require ( './binaries/win32-ia32.node' );
+
+} else if ( platform === 'linux' && arch === 'x64' ) {
+
+  module.exports = require ( './binaries/linux-x64.node' );
+
+} else {
+
+  throw new Error ( '[NSFW] Unsupported platform or architecture' );
+
+}
